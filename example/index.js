6fe275e4a19afd915ba7feeea81553cced1f0fbe
tenapee <tena.percy@gmail.com>
2017-03-04 03:58:32 -0800

    Create Lightbox component with logic and styles

--- example/index.js
+++ example/index.js
@@ -4,40 +4,86 @@ import ReactDOM from 'react-dom';
 import Lightbox from 'components/Lightbox';
 import Button from 'components/Button';
 
-const dest = document.getElementById('content');
+import image1 from 'images/cute-puppy.jpg';
+import image2 from 'images/pug.jpg';
+import image3 from 'images/pupper.jpg';
+import image4 from 'images/butterfly-pup.jpg';
+import image5 from 'images/disguise-dog.jpg';
+import image6 from 'images/doggo.jpg';
 
+const images = [{
+  src: image1,
+  caption: 'Look at this cute tea-pup!',
+}, {
+  src: image2,
+  caption: 'I\'m a snug little pug',
+}, {
+  src: image3,
+  caption: 'Can you not?',
+}, {
+  src: image4,
+  caption: 'Ugh. Mom. No.',
+}, {
+  src: image5,
+  caption: 'Going undercover to figure out who\'s a good boy',
+}, {
+  src: image6,
+  caption: 'Did I hear a cheese wrapper?',
+}];
+
+const dest = document.getElementById('content');
 
 class LightboxExample extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      photo: 0,
+      imageIndex: 0,
       isOpen: false
     };
 
-    this.onButtonClick = this.onButtonClick.bind(this);
+    this.onOpenClick = this.onOpenClick.bind(this);
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.onClickNext = this.onClickNext.bind(this);
+    this.onClickPrev = this.onClickPrev.bind(this);
   }
 
-  onButtonClick() {
-    debugger
+  onOpenClick() {
     this.setState({ isOpen: true });
+  }
+
+  closeLightbox() {
+    this.setState({ isOpen: false });
+  }
+
+  onClickNext() {
+    this.setState({ imageIndex: (this.state.imageIndex + 1) % images.length });
+  }
+
+  onClickPrev() {
+    this.setState({ imageIndex: (this.state.imageIndex + images.length - 1) % images.length });
   }
 
   render() {
     const {
-      photo,
+      imageIndex,
       isOpen,
+
     } = this.state;
 
     return (
       <div>
         <Button
           label="Open Lightbox"
-          onClick={this.onButtonClick}
+          onClick={this.onOpenClick}
         />
         <Lightbox
           isOpen={isOpen}
+          onClose={this.closeLightbox}
+          images={images}
+          onClickNext={this.onClickNext}
+          onClickPrev={this.onClickPrev}
+          imageIndex={imageIndex}
         />
 
       </div>
--- src/components/Lightbox.js
+++ src/components/Lightbox.js
@@ -1,39 +1,168 @@
 import React, { Component, PropTypes } from 'react';
 import cssModules from 'react-css-modules';
 import Modal from 'react-modal';
+import Button from './Button';
 
 const styles = require('../styles/lightbox.css');
 
-const customStyles = {
-  content : {
-    top                   : '50%',
-    left                  : '50%',
-    right                 : 'auto',
-    bottom                : 'auto',
-    marginRight           : '-50%',
-    transform             : 'translate(-50%, -50%)'
-  }
-};
+const PREV_KEY = 37;
+const NEXT_KEY = 39;
+const ESC_KEY = 27;
 
 class Lightbox extends Component {
   constructor(props) {
     super(props);
 
+    this.handleNext = this.handleNext.bind(this);
+    this.handlePrev = this.handlePrev.bind(this);
+    this.handleKeydown = this.handleKeydown.bind(this);
+  }
+
+  componentDidMount () {
+    if (this.props.isOpen && this.props.enableKeyboardInput) {
+      window.addEventListener('keydown', this.handleKeydown);
+    }
+  }
+
+  componentWillReceiveProps (nextProps) {
+
+    if (nextProps.preloadNextImage) {
+      const nextIndex = nextProps.imageIndex + 1;
+      const prevIndex = nextProps.imageIndex - 1;
+
+      this.preloadImage(prevIndex);
+      this.preloadImage(nextIndex);
+    }
+
+    if (!this.props.isOpen && nextProps.isOpen && nextProps.enableKeyboardInput) {
+      window.addEventListener('keydown', this.handleKeydown);
+    }
+    if (!nextProps.isOpen && nextProps.enableKeyboardInput) {
+      window.removeEventListener('keydown', this.handleKeydown);
+    }
+  }
+
+  componentWillUnmount () {
+    if (this.props.enableKeyboardInput) {
+      window.removeEventListener('keydown', this.handleKeydown);
+    }
+  }
+
+  preloadImage (index) {
+    const image = this.props.images[index];
+
+    if (!image) return;
+
+    const img = new Image();
+
+    img.src = image.src;
+  }
+
+  handleNext (event) {
+    if (event) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    this.props.onClickNext();
+  }
+
+  handlePrev (event) {
+    if (event) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    this.props.onClickPrev();
+  }
+
+  handleKeydown (event) {
+    switch(event.keyCode) {
+      case PREV_KEY:
+        this.handlePrev(event);
+        return true;
+      case NEXT_KEY:
+        this.handleNext(event);
+        return true;
+      case ESC_KEY:
+        this.props.onClose();
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  renderImages () {
+    const {
+      imageIndex,
+      images,
+    } = this.props;
+
+    if (!images || !images.length) return null;
+
+    const image = images[imageIndex];
+
+    return (
+      <figure>
+        <img
+          styleName='image'
+          alt={image.alt}
+          src={image.src}
+        />
+        <div styleName="caption">
+          <div styleName="caption-content">
+            <div styleName="caption-body">{image.caption}</div>
+            <div styleName="photo-count">{imageIndex + 1} of {images.length}</div>
+          </div>
+        </div>
+      </figure>
+    );
   }
 
   render() {
-    const { isOpen } = this.props;
-    const noop = () => {};
+    const {
+      closeOnClick,
+      isOpen,
+      onClose,
+    } = this.props;
+
+    const modalStyle = {
+      overlay: {
+        zIndex: 1000,
+        backgroundColor: 'transparent',
+      },
+      content: {
+        backgroundColor: 'transparent',
+        overflow: 'hidden',
+        border: 'none',
+        borderRadius: 0,
+        padding: 0,
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        opacity: 1,
+      },
+    };
 
     return (
       <div styleName="lightbox">
         <Modal
           isOpen={isOpen}
-          onRequestClose={noop}
-          onAfterOpen={() => this.outerEl && this.outerEl.focus()}
-          style={customStyles}
+          onRequestClose={onClose}
+          style={modalStyle}
+          contentLabel='Lightbox'
         >
-          <div>I am a modal!!!</div>
+          <div styleName="outer">
+            <div styleName="inner"  onClick={!!closeOnClick && onClose}>
+              <div styleName="navigation">
+                <Button label="X" onClick={onClose} />
+                <Button label="< Prev" type="" onClick={this.handlePrev} />
+                <Button label="Next >" type="" onClick={this.handleNext} />
+              </div>
+              {this.renderImages()}
+            </div>
+          </div>
         </Modal>
       </div>
     );
@@ -41,7 +170,10 @@ class Lightbox extends Component {
 }
 
 Lightbox.defaultProps = {
-
+  imageIndex: 0,
+  enableKeyboardInput: true,
+  preloadNextImage: true,
+  closeOnClick: false,
 };
 
 Lightbox.propTypes = {
